e46a924e4d7a8b952e4a0cba905793538a6be360
GabrielPavaloiu <gabrieln.pavaloiu@gmail.com>
2024-10-07 13:09:57 +0300

    lab1

--- 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -1,7 +1,15 @@
 fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
-    // TODO
+    let mut mx = input[0];
+    let mut mn = input[1];
+    for ele in input {
+        if ele > mx {
+            mx = ele
+        } else if ele < mn {
+            mn = ele
+        }
+    }
 
-    println!("{} is largest and {} is smallest");
+    println!("{} is largest and {} is smallest", mx, mn);
 }
